8369a9f497f597de2050117f26779145e0f7f957
Sid <sidvekariya20@gmail.com>
2023-08-28 09:31:22 +0530

    button component created

---
+++ components/Button.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import styles from "../styles/button.module.css";
+import { Work_Sans } from "@next/font/google";
+import { Playfair_Display } from "@next/font/google";
+
+const worksans = Work_Sans({ subsets: ["latin"] });
+const PlayfairDisplay = Playfair_Display({ subsets: ["latin"] });
+
+const Button = ({ text, backgroundColor }) => {
+    return (
+        <button
+            style={worksans.style}
+            className={`${styles.button} ${
+                backgroundColor === "green" ? styles.green : styles.white
+            }`}
+        >
+            {text}
+        </button>
+    );
+};
+
+export default Button;
