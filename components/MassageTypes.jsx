5e09f7a2ea590e3fd4f4ff369e8857c0cfa56186
Sid <sidvekariya20@gmail.com>
2023-08-29 07:36:19 +0530

    massageTypes section completed

---
+++ components/MassageTypes.jsx
@@ -0,0 +1,63 @@
+import React from "react";
+import styles from "../styles/massageTypes.module.css";
+import { BsArrowRight } from "react-icons/bs";
+import { massageData } from "@/utils/massageData";
+import Image from "next/image";
+import { Work_Sans } from "@next/font/google";
+import { Playfair_Display } from "@next/font/google";
+
+const worksans = Work_Sans({ subsets: ["latin"] });
+const PlayfairDisplay = Playfair_Display({ subsets: ["latin"] });
+
+const Card = ({ massage }) => {
+    return (
+        <div className={styles.card}>
+            <h2 style={PlayfairDisplay.style} className={styles.name}>
+                {massage.massageName}
+            </h2>
+            <Image
+                className={styles.line}
+                src={massage.line}
+                alt=""
+                width={300}
+                height={200}
+            />
+            <div style={worksans.style} className={styles.both}>
+                <p className={styles.price}>Rp. {massage.price}</p>
+                <p className={styles.number}> {massage.massageNumber}</p>
+            </div>
+            <Image
+                className={styles.media}
+                src={massage.media}
+                alt={massage.massageName}
+                width={300}
+                height={200}
+            />
+            <div style={worksans.style} className={styles.end}>
+                <div className={styles.text}>Select package</div>
+                <div className={styles.arrow}>
+                    <BsArrowRight />
+                </div>
+            </div>
+        </div>
+    );
+};
+
+const MassageTypes = () => {
+    return (
+        <>
+            <div className={styles.cardList}>
+                {massageData.map((massage) => (
+                    <Card key={massage.id} massage={massage} />
+                ))}
+            </div>
+            <div>
+                <div className={styles.serviceButton}>
+                    <button className={styles.btn}>More services</button>
+                </div>
+            </div>
+        </>
+    );
+};
+
+export default MassageTypes;
